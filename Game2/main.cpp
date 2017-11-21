003a414ef0c59239cb54cc1b535286fe511efaf0
groot <groot@LAPTOP-P7GPBBKF>
2017-11-20 23:06:13 -0800

    Add project files.

---
+++ Game2/main.cpp
@@ -0,0 +1,42 @@
+#include <SFML\Graphics.hpp>
+
+int main(void) {
+
+	sf::RenderWindow window(sf::VideoMode(600, 600), "Test!");
+	sf::RectangleShape shape;
+	shape.setSize(sf::Vector2f(100, 100));
+	shape.setFillColor(sf::Color::Blue);
+	shape.setPosition(sf::Vector2f(300, 300));
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Up) {
+					shape.move(sf::Vector2f(0, -5));
+				}
+				if (event.key.code == sf::Keyboard::Right) {
+					shape.move(sf::Vector2f(5, 0));
+				}
+				if (event.key.code == sf::Keyboard::Down) {
+					shape.move(sf::Vector2f(0, 5));
+				}
+				if (event.key.code == sf::Keyboard::Left) {
+					shape.move(sf::Vector2f(-5, 0));
+				}
+			}
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+
+	return(0);
+}
